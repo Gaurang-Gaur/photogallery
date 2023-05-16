b9968cdbff036ccb5b3deb00186dc392e309ad3f
Gaurang-Gaur <gauranggaur007@gmail.com>
2023-05-16 16:14:21 +0530

    update

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app=express();
+const port=5000 || process.env.PORT;
+const mongoose=require("mongoose");
+
+const cors=require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+const UploadRoutes=require("./routes/UploadRoutes");
+
+
+app.use(UploadRoutes)
+mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("mongodb connected");}).catch((err)=>{console.log(err);})
+
+
+app.listen(port,function(){
+    console.log(`server is listening at: ${port}`);
+})
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+const Navbar=()=>{
+
+return (
+    <nav>
+    <div className="nav__logo">Poster Palace</div>
+  </nav>
+)
+}
+
+
+
+
+
+
+
+export default Navbar;
